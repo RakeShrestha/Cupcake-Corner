0f8114dd7053b013333b50a7ae43fe03c7c19c26
RakeShrestha <orakeshstha7@gmail.com>
2025-01-08 23:06:17 +0545

    Initial Commit

---
+++ Cupcake Corner/Cupcake_CornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cupcake_CornerApp.swift
+//  Cupcake Corner
+//
+//  Created by Rakesh Shrestha on 08/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct Cupcake_CornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
