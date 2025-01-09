c8d5874c89d2719a6c7e36371064cec598c7fe6a
RakeShrestha <orakeshstha7@gmail.com>
2025-01-09 21:03:49 +0545

    Made Menu Screen, UserDetails Screen and Checkout Screen

---
+++ Cupcake Corner/Order.swift
@@ -0,0 +1,56 @@
+//
+//  Order.swift
+//  Cupcake Corner
+//
+//  Created by Rakesh Shrestha on 09/01/2025.
+//
+
+import Foundation
+
+@Observable
+class Order {
+    static let types = ["Vanilla", "Strawberry", "Chocolate", "Rainbow"]
+    
+    var type = 0
+    var quantity = 3
+    
+    var specialRequestEnabled = false {
+        didSet {
+            if !specialRequestEnabled {
+                extraFrosting = false
+                addSprinkles = false
+            }
+        }
+    }
+    var extraFrosting = false
+    var addSprinkles = false
+    
+    var name = ""
+    var streetAddress = ""
+    var city = ""
+    var zip = ""
+    
+    var hasValidAddress: Bool {
+        if name.isEmpty || streetAddress.isEmpty || city.isEmpty || zip.isEmpty {
+            return false
+        }
+        return true
+    }
+    
+    var cost: Decimal {
+        var cost = Decimal(quantity) * 2
+        
+        cost += Decimal(type) / 2
+        
+        if extraFrosting {
+            cost += Decimal(quantity)
+        }
+        
+        if addSprinkles {
+            cost += Decimal(quantity) / 2
+        }
+        
+        return cost
+    }
+    
+}
